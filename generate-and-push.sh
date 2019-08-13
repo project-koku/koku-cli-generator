683f9e30e6f0f80f2635e7c15f22b228ebdbfe9c
Brad Smith <brasmith@redhat.com>
2019-08-13 12:01:47 -0400

    clean up environment variable handling

--- generate-and-push.sh
+++ generate-and-push.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-unset FAIL
 # Requires the following environment variables to be set:
-[ -z "${GITHUB_USERNAME}" ] && echo "missing GITHUB_USERNAME" && FAIL=1
-[ -z "${GITHUB_PASSWORD}" ] && echo "missing GITHUB_PASSWORD" && FAIL=1
-[ -z "${COMMITTER_EMAIL}" ] && echo "missing COMMITTER_EMAIL" && FAIL=1
-[ -z "${REPO_BRANCH}" ] && echo "missing REPO_BRANCH" && FAIL=1
-[ -z "${API_AUTH}" ] && echo "missing API_AUTH" && FAIL=1
-[ -z "${API_URL}" ] && echo "missing API_URL" && FAIL=1
+REQUIRED=(
+    API_AUTH_USERNAME API_AUTH_PASSWORD API_JSON_URL
+    COMMITTER_EMAIL COMMITTER_NAME REPO_BRANCH
+    GITHUB_USERNAME GITHUB_PASSWORD
+)
+unset FAIL
+for VAR_NAME in ${REQUIRED[@]}; do
+    [ -z "${!VAR_NAME}" ] && echo "missing " ${!VAR_NAME} && FAIL=1
+done
 # If any of those were not set, exit early.
 [ -z "${FAIL}" ] || exit 1
 
 # Reasonable defaults of these environment variables are not set:
-[ -z "${COMMITTER_NAME}" ] && COMMITTER_NAME="koku-code-bot"
 [ -z "${GITHUB_REPO_PATH}" ] && GITHUB_REPO_PATH="project-koku/koku-cli"
 [ -z "${GENERATOR_TYPE}" ] && GENERATOR_TYPE="bash"
 
@@ -24,7 +25,7 @@ GENERATE_DIR="${REPO_DIR}/client-${GENERATOR_TYPE}"
 set -e
 
 # Pull down the openapi.json specification.
-curl -u "${API_AUTH}" "${API_URL}" -o "${API_JSON}"
+curl -u "${API_AUTH_USERNAME}:${API_AUTH_PASSWORD}" "${API_JSON_URL}" -o "${API_JSON}"
 
 # Prepare the repo clone.
 git clone "${REPO_URL}" "${REPO_DIR}"
@@ -41,5 +42,5 @@ git rm -r --force --ignore-unmatch "${GENERATE_DIR}"
 
 # Commit and push any changes up to GitHub.
 git add "${GENERATE_DIR}"
-git commit -m "generate ${GENERATOR_TYPE} code based on latest ${API_URL}"
+git commit -m "generate ${GENERATOR_TYPE} code based on latest ${API_JSON_URL}"
 git push origin "${REPO_BRANCH}"

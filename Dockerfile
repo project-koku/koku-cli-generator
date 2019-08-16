c74a4cbdf657f992519ddff3c0028d6a8a08ca52
Ilya White <ilya.white@redhat.com>
2019-08-16 10:03:58 -0400

    Stop pushing to repo, create a release instead.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM openapitools/openapi-generator-cli:latest
 
 RUN set -x && \
-    apk add --no-cache git curl
+    apk add --no-cache git curl jq
 
 COPY generate-and-push.sh /usr/local/bin/
 RUN chmod 755 /usr/local/bin/generate-and-push.sh
--- generate-and-push.sh
+++ generate-and-push.sh
@@ -2,7 +2,6 @@
 # Requires the following environment variables to be set:
 REQUIRED=(
     API_AUTH_USERNAME API_AUTH_PASSWORD API_JSON_URL
-    COMMITTER_EMAIL COMMITTER_NAME REPO_BRANCH
     GITHUB_USERNAME GITHUB_PASSWORD
 )
 unset FAIL
@@ -13,13 +12,11 @@ done
 [ -z "${FAIL}" ] || exit 1
 
 # Reasonable defaults of these environment variables are not set:
-[ -z "${GITHUB_REPO_PATH}" ] && GITHUB_REPO_PATH="project-koku/koku-cli"
-[ -z "${GENERATOR_TYPE}" ] && GENERATOR_TYPE="bash"
+[ -z "${VERSION}" ] && VERSION=`date +%s`
 
 API_JSON="/tmp/openapi.json"
 REPO_DIR="/tmp/koku-cli-git"
 REPO_URL="https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/${GITHUB_REPO_PATH}.git"
-GENERATE_DIR="${REPO_DIR}/client-${GENERATOR_TYPE}"
 
 # If any command fails, exit early.
 set -e
@@ -27,20 +24,17 @@ set -e
 # Pull down the openapi.json specification.
 curl -u "${API_AUTH_USERNAME}:${API_AUTH_PASSWORD}" "${API_JSON_URL}" -o "${API_JSON}"
 
-# Prepare the repo clone.
-git clone "${REPO_URL}" "${REPO_DIR}"
-cd "${REPO_DIR}"
-git config --local user.email "${COMMITTER_EMAIL}"
-git config --local user.name "${COMMITTER_NAME}"
+# Create a new release in Github.
+RELEASE_RESPONSE=`curl -u "${GITHUB_USERNAME}:${GITHUB_PASSWORD}" -X POST https://api.github.com/repos/project-koku/koku-cli/releases -d '{"tag_name":"'"${VERSION}"'", "name":"CLI Release '"${VERSION}"'"}'`
+RELEASE_ID=`echo $RELEASE_RESPONSE | jq .id`
+RELEASE_UPLOAD_URL=`echo $RELEASE_RESPONSE | jq .upload_url | sed 's/{?name,label}//' | sed -r 's/"//gI'`
 
-# Prepare the branch for this build.
-git checkout "${REPO_BRANCH}" || git checkout --track -b "${REPO_BRANCH}"
-git rm -r --force --ignore-unmatch "${GENERATE_DIR}"
-
-# Hand control over to openapi-generator's entrypoint script.
-/usr/local/bin/docker-entrypoint.sh generate -i "${API_JSON}" -g "${GENERATOR_TYPE}" -o "${GENERATE_DIR}"
-
-# Commit and push any changes up to GitHub.
-git add "${GENERATE_DIR}"
-git commit -m "generate ${GENERATOR_TYPE} code" -m "based on latest ${API_JSON_URL}"
-git push origin "${REPO_BRANCH}"
+# Generate our clients, and upload to the github release.
+for GENERATOR_TYPE in bash python ruby java go
+do
+    GENERATE_DIR="${REPO_DIR}/client-${GENERATOR_TYPE}"
+    CLIENT_ARCHIVE_NAME=${GENERATOR_TYPE}-client.tar.gz
+    /usr/local/bin/docker-entrypoint.sh generate -i "${API_JSON}" -g "${GENERATOR_TYPE}" -o "${GENERATE_DIR}"
+    tar -czvf ${CLIENT_ARCHIVE_NAME} ${GENERATE_DIR}
+    curl -u "${GITHUB_USERNAME}:${GITHUB_PASSWORD}" -X POST "${RELEASE_UPLOAD_URL}?name=${CLIENT_ARCHIVE_NAME}" -H Content-Type:application/tar+gzip --data-binary "@${CLIENT_ARCHIVE_NAME}"
+done
